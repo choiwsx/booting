41516e170b8a48ae24861f53cfc8dbfef6296f88
Kohalahal <52988648+kohalahal@users.noreply.github.com>
2020-07-30 17:38:05 +0900

    hal

--- src/main/java/org/kitchen/booting/config/MVCConfig.java
+++ src/main/java/org/kitchen/booting/config/MVCConfig.java
@@ -8,9 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MVCConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("index");
-        registry.addViewController("/login").setViewName("login");
-        registry.addViewController("/index").setViewName("index");
+//        registry.addViewController("/").setViewName("index");
+//        registry.addViewController("/login").setViewName("login");
+//        registry.addViewController("/index").setViewName("index");
     }
 
 }
---
+++ src/main/java/org/kitchen/booting/controller/HomeController.java
@@ -0,0 +1,7 @@
+package org.kitchen.booting.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+}
