7d1663a91d12978653acbac83b2d1e7e9a1b5b61
Kohalahal <52988648+kohalahal@users.noreply.github.com>
2020-08-05 22:58:32 +0900

    home model

--- src/main/java/org/kitchen/booting/controller/HomeController.java
+++ src/main/java/org/kitchen/booting/controller/HomeController.java
@@ -1,17 +1,27 @@
 package org.kitchen.booting.controller;
 
 import org.kitchen.booting.domain.userauth.User;
+import org.kitchen.booting.service.RecipeService;
+import org.kitchen.booting.service.TagService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-    @GetMapping(value="/")
-    public String indexView(@AuthenticationPrincipal User user)
-    {
+    @Autowired
+    RecipeService recipeService;
+    @Autowired
+    TagService tagService;
 
+    @GetMapping(value="/")
+    public String indexView(@AuthenticationPrincipal User user, Model model)
+    {
+        model.addAttribute("recipes", recipeService.findAll());
+        model.addAttribute("tags",tagService.randomTagList());
         return "index";
     }
 }
