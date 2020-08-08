f3a93a8c74ed27dae194b24f4a0ebe9c1a102c30
Wonseok Choi <choiwsx@nate.com>
2020-08-08 23:20:54 +0900

    commit for merge

--- src/main/java/org/kitchen/booting/controller/RecipeController.java
+++ src/main/java/org/kitchen/booting/controller/RecipeController.java
@@ -2,6 +2,7 @@ package org.kitchen.booting.controller;
 
 import org.kitchen.booting.domain.*;
 import org.kitchen.booting.domain.userauth.User;
+import org.kitchen.booting.repository.CategoryRepository;
 import org.kitchen.booting.service.LikeService;
 import org.kitchen.booting.service.CommentService;
 import org.kitchen.booting.service.RecipeService;
@@ -33,6 +34,8 @@ public class RecipeController {
     ScrapService scrapService;
     @Autowired
     LikeService likeService;
+    @Autowired
+    CategoryRepository categoryRepository;
 
     CommentService commentService;
     private final Logger logger = LoggerFactory.getLogger(RecipeController.class);
@@ -40,7 +43,8 @@ public class RecipeController {
 
     @GetMapping("/recipe/register")
     public String recipeForm(@ModelAttribute Recipe recipe, Model model){
-//        model.addAttribute("recipeVO", recipe);
+        List<Category> categoryList = categoryRepository.findAll();
+        model.addAttribute("category", categoryList);
         return "/recipe/register";
     }
 //    @PostMapping("/recipe/register")
@@ -60,27 +64,29 @@ public class RecipeController {
     public String get(@AuthenticationPrincipal User user,
             @PathVariable("recipeNo") Long recipeNo,Model model) {
         Recipe recipe = recipeService.findByRecipeNo(recipeNo);
-        Scrap scrap = scrapService.getScrap(user.getUserId(), recipeNo);
-        Like like = likeService.getLike(user.getUserId(), recipeNo);
-        List<Profile> count = likeService.listByRecipeNo(recipeNo);
+        recipe.getSteps().sort((a, b)-> a.getStepNo().compareTo(b.getStepNo()));
+        recipe.getIngredients().sort((a,b)->a.getIngredientNo().compareTo(b.getIngredientNo()));
+//        Scrap scrap = scrapService.getScrap(user.getUserId(), recipeNo);
+//        Like like = likeService.getLike(user.getUserId(), recipeNo);
+//        List<Profile> count = likeService.listByRecipeNo(recipeNo);
         List<String> recipeTag = recipeService.CheckTag(recipeNo);
         if (recipe != null) {
-            if (count == null || recipeService.CheckTag(recipeNo) == null) {
+//            if (count == null || recipeService.CheckTag(recipeNo) == null) {
                 model.addAttribute("recipe", recipe);
-                model.addAttribute("scrap", scrap);
-                model.addAttribute("like", like);
+//                model.addAttribute("scrap", scrap);
+//                model.addAttribute("like", like);
                 model.addAttribute("recipeTag", recipeTag); //레시피 태그
 //                model.addAttribute("allLike", likeService.findAll());
-                model.addAttribute("counts", count);
-            } else {
-                model.addAttribute("recipe", recipe);
-                model.addAttribute("recipeTag", recipeService.CheckTag(recipeNo));
-                model.addAttribute("scrap", scrap);
-                model.addAttribute("like", like);
-                model.addAttribute("recipeTag", recipeTag); //레시피 태그
+//                model.addAttribute("counts", count);
+//            } else {
+//                model.addAttribute("recipe", recipe);
+//                model.addAttribute("recipeTag", recipeService.CheckTag(recipeNo));
+//                model.addAttribute("scrap", scrap);
+//                model.addAttribute("like", like);
+//                model.addAttribute("recipeTag", recipeTag); //레시피 태그
 //                model.addAttribute("allLike", likeService.findAll());
-                model.addAttribute("counts", count);
-            }
+//                model.addAttribute("counts", count);
+//            }
         }
         return "recipe/get";
     }
@@ -118,4 +124,35 @@ public class RecipeController {
             model.addAttribute("title", recipeService.findByRecipeNo(recipeNo).getTitle());
             return "/recipe/likelist";
         }
+
+        @RequestMapping(value="/recipe/getSubCategories", method=RequestMethod.GET)
+        public @ResponseBody List<Category> findSubCategory(
+                @RequestParam(value ="prevCateNo", required = true) Long prevCateNo){
+            Optional<Category> mainCategory = categoryRepository.findById(prevCateNo);
+            List<Category> allCategory =  new ArrayList<>();
+            List<Category> subCategories = new ArrayList<>();
+            if(mainCategory.isPresent())
+            {
+                allCategory = categoryRepository.findAll();
+                for(int i=0; i<allCategory.size(); i++)
+                {
+                    if(allCategory.get(i).getMainCategory()!=null) {
+                        if(allCategory.get(i).getMainCategory().getCategoryNo().equals(prevCateNo))
+                        {
+                            logger.info("!!");
+                            subCategories.add(allCategory.get(i));
+
+                        }
+                    }
+                }
+                logger.info("@@@서브카테고리"+subCategories);
+            }
+            subCategories.forEach(sub->sub.setSubCategories(null));
+            subCategories.forEach(sub->sub.getMainCategory().setSubCategories(null));
+            subCategories.forEach(sub->sub.getMainCategory().setRecipes(null));
+            subCategories.forEach(sub->sub.setRecipes(null));
+
+            return subCategories;
+        }
+
     }
--- src/main/java/org/kitchen/booting/repository/CategoryRepository.java
+++ src/main/java/org/kitchen/booting/repository/CategoryRepository.java
@@ -3,6 +3,10 @@ package org.kitchen.booting.repository;
 import org.kitchen.booting.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    public List<Category> findByMainCategory(Category mainCategory);
 
 }
